3dc75d4ec02803e4f3dccfa61ff2226d2c8797b6
Brian Bycynski <high5bri@hotmail.com>
2018-06-16 15:40:23 -0400

    finished lecture 28

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-// Old key
-// const API_KEY = 'AIzaSyAhjW8XgUxytacj3ReGvU862GZq1P8Qhss';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // New Key
 const API_KEY = 'AIzaSyCSp9YJiat-PsAYFD0X70XCXuC476madv4';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// Old key
+// const API_KEY = 'AIzaSyAhjW8XgUxytacj3ReGvU862GZq1P8Qhss';
